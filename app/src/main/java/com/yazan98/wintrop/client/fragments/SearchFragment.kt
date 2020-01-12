e215338ad18c95500aa104fcc163b8364742f084
Yazan Tarifi <yazantarifi98@gmail.com>
2020-01-12 20:28:54 +0000

    finish splash screen design

--- app/src/main/java/com/yazan98/wintrop/client/fragments/SearchFragment.kt
+++ app/src/main/java/com/yazan98/wintrop/client/fragments/SearchFragment.kt
@@ -1,5 +1,7 @@
 package com.yazan98.wintrop.client.fragments
 
+import android.view.View
+import com.yazan98.wintrop.client.R
 import io.vortex.android.ui.fragment.VortexBaseFragment
 import io.vortex.android.ui.fragment.VortexFragment
 import javax.inject.Inject
@@ -7,5 +9,9 @@ import javax.inject.Inject
 class SearchFragment @Inject constructor() : VortexBaseFragment() {
     override fun getLayoutRes(): Int {
         return R.layout.fragment_search
+    }
+
+    override fun initScreen(view: View) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
--- app/src/main/java/com/yazan98/wintrop/client/screens/SplashScreen.kt
+++ app/src/main/java/com/yazan98/wintrop/client/screens/SplashScreen.kt
@@ -3,6 +3,9 @@ package com.yazan98.wintrop.client.screens
 import android.os.Bundle
 import com.yazan98.wintrop.client.R
 import io.vortex.android.ui.activity.VortexScreen
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class SplashScreen : VortexScreen() {
 
@@ -12,7 +15,13 @@ class SplashScreen : VortexScreen() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        ExploreButton?.apply {
+            this.setOnClickListener {
+                GlobalScope.launch {
+                    startScreen<MainScreen>(true)
+                }
+            }
+        }
     }
 
 }
